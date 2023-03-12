db7b0b21c74434b2c64a7602617452e299f4d38f
achicago <achigonye@gmail.com>
2023-03-12 14:10:38 +0100

    Nav Header setup

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Text } from 'react-native';
+
+import WelcomeScreen from './screens/WelcomeScreen';
+import UserScreen from './screens/UserScreen';
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName='User'>
+        <Drawer.Screen name='Welcome' component={WelcomeScreen}
+          options={{
+            headerStyle: { backgroundColor: '#3c0a6b' },
+            headerTintColor: 'white',
+          }}
+        />
+        <Drawer.Screen name='User' component={UserScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
